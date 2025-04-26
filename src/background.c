01124c6869afbdaaf246a4194d429a77da0bc80a
Gabriel Ford <gabe@fordltc.net>
2025-04-26 19:23:28 -0400

    Clean up background shader.

--- src/background.c
+++ src/background.c
@@ -23,8 +23,12 @@ static const char *vert_shader_src =
     "\n"
     "void main()\n"
     "{\n"
-		// We need to apply the offset after scaling occurs to prevent scaling shooting the tile offscreen.
-    "    gl_Position = projection * ((model * vec4(aPos, 0.0, 1.0)) + vec4(150.0 * gl_InstanceID, 0, 0, 0));\n"
+    "    vec4 tileOffset = vec4(150.0 * gl_InstanceID, 0, 0, 0)\n;"
+    // We need to apply the offset after transformation occurs to prevent 
+    // scaling shooting the tile offscreen.
+    "    vec4 worldPosition = model * vec4(aPos, 0.0, 1.0);\n"
+    "    vec4 offsetWorldPosition = worldPosition + tileOffset;\n"
+    "    gl_Position = projection * offsetWorldPosition;\n"
     "    texCoord = vec2(aTexCoord.x * (64.0 / 4096.0), aTexCoord.y * (64.0 / 576.0));\n"
     "}\n";
 
