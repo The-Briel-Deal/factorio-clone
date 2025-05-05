3427a01e604c524a505eaca6448b82f99a51ceb2
Gabriel Ford <gabe@fordltc.net>
2025-05-05 18:19:52 -0400

    Remove explicit locations for position and tex coord.

--- shader/player_vert.glsl
+++ shader/player_vert.glsl
@@ -1,6 +1,6 @@
 #version 450 core
-layout(location = 0) in vec2 aPos;
-layout(location = 1) in vec2 aTexCoord;
+in vec2 aPos;
+in vec2 aTexCoord;
 layout(std140) uniform Matrices {
   mat4 projection;
   mat4 model;
