ace04bf511c1672c92ddebb0eab82fb56555ecf5
Gabriel Ford <gabe@fordltc.net>
2025-05-09 18:16:13 -0400

    Get noise nearly working.

--- src/noise.c
+++ src/noise.c
@@ -5,12 +5,41 @@ float dotf(float xa, float ya, float xb, float yb) {
 }
 
 float frandom(vec2s v) {
-  return fabsf(fmodf(sinf(dotf(v.x, v.y, 12.9898, 78.233)) * 43758.5453123, 1.0));
+  return fabsf(
+      fmodf(sinf(dotf(v.x, v.y, 12.9898, 78.233)) * 43758.5453123, 1.0));
+}
+
+float mix(float a, float b, float fract) {
+  assert(fract <= 1.0);
+  assert(fract >= 0.0);
+  a *= fract;
+  b *= (1 - fract);
+
+  return a + b;
 }
 
 float gf_noise(vec2s v, float freq) {
-	v.x *= freq;
-	v.y *= freq;
-  return frandom(v);
-}
+  v.x *= freq;
+  v.y *= freq;
 
+  float top   = ceilf(v.y);
+  float bot   = floorf(v.y);
+  float right = ceilf(v.x);
+  float left  = floorf(v.x);
+
+  vec2s top_left  = {top, left};
+  vec2s top_right = {top, right};
+  vec2s bot_left  = {bot, left};
+  vec2s bot_right = {bot, right};
+
+  float top_left_rand  = frandom(top_left);
+  float top_right_rand = frandom(top_right);
+  float bot_left_rand  = frandom(bot_left);
+  float bot_right_rand = frandom(bot_right);
+
+  float top_mixed = mix(top_left_rand, top_right_rand, v.x - left);
+  float bot_mixed = mix(bot_left_rand, bot_right_rand, v.x - left);
+  float mixed     = mix(top_mixed, bot_mixed, v.y - bot);
+
+  return mixed;
+}
