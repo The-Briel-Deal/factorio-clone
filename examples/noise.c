f8a2069bc7fefffb8fd4fd10e91ee8d610e0d4a4
Gabriel Ford <gabe@fordltc.net>
2025-05-09 17:45:22 -0400

    Add freq to gf_noise()

--- examples/noise.c
+++ examples/noise.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
   for (y = 0; y < 50; y++) {
     printf("|");
     for (x = 0; x < 50; x++) {
-      int val = floorf(gf_noise((vec2s){x, y}, 1.0) * 255);
+      int val = floorf(gf_noise((vec2s){x, y}, 0.1) * 255);
       printf("\x1b[48;2;%i;%i;%im \x1b[0m", val, val, val);
     }
     printf("|\n");
