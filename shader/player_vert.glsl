ff4de7f1b369e4beba17ed4e476a82cc041ed9d7
Gabriel Ford <gabe@fordltc.net>
2025-05-11 11:42:51 -0400

    Got player shader rendering character sprite.

--- shader/player_vert.glsl
+++ shader/player_vert.glsl
@@ -5,9 +5,22 @@ layout(std140) uniform Matrices {
   mat4 projection;
   mat4 model;
 };
+uniform int spriteIndex = 80;
+
 out vec2 texCoord;
 
+// The player sprite sheet 22x8 sprites.
+#define PLAYER_SPRITE_SHEET_CELL_COUNT_WIDTH  22
+#define PLAYER_SPRITE_SHEET_CELL_COUNT_HEIGHT 8
+
 void main() {
+  vec2 offset = vec2(spriteIndex % PLAYER_SPRITE_SHEET_CELL_COUNT_WIDTH,
+                     spriteIndex / PLAYER_SPRITE_SHEET_CELL_COUNT_WIDTH);
+  offset /= vec2(PLAYER_SPRITE_SHEET_CELL_COUNT_WIDTH,
+                 PLAYER_SPRITE_SHEET_CELL_COUNT_HEIGHT);
+
   gl_Position = projection * model * vec4(aPos, 0.0, 1.0);
-  texCoord    = aTexCoord;
+  texCoord    = vec2((aTexCoord.x / PLAYER_SPRITE_SHEET_CELL_COUNT_WIDTH),
+                     aTexCoord.y / PLAYER_SPRITE_SHEET_CELL_COUNT_HEIGHT) +
+             offset;
 }
